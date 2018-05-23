0f0f05f0a77b6caf60269deece7ece822a40e612
Andrea Campanella <andrea@digiconsoles.com>
2018-05-23 09:59:48 +0100

    fixed directory missmatch

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!bash
+#working_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
 sudo apt-get update 
 sudo apt-get install -y git-core zsh mc vim screen terminator htop openssh-server
 #let's install zsh
@@ -8,14 +10,14 @@ chsh -s $(which zsh)
 git clone https://github.com/powerline/fonts.git --depth=1
 sh fonts/install.sh
 rm -rf fonts
-#terminator configuration
+#terminator configuration and zshrc
 cp -rf terminator ~/.config/terminator
+cp zshrc ~/.zshrc
 #install plugins
 cd ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions
-#install zshrc
-cp zshrc ~/.zshrc
+
 
 echo "please log out and in or reboot the system"
 
