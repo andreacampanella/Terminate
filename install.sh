2a3cbf007c4cc16e8e133ea185b02ea21b1201c8
emuboy <emuboy85@gmail.com>
2018-07-20 11:49:37 +0100

    fixed typoos

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!bash
 #working_dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 #in case we have no arguments, help is needed
-if [[ $# -eq 0 ]] ; then
+if [ -z "$*" ]; then 
     echo 'install.sh -w or --warm to install terminator with warm colour palette'
     echo 'install.sh -c or --cold to install terminator with cold colour palette'
     echo 'install.sh ---cli_only to install in a commandline only env'
     exit 0
-fi [$]
+fi 
 
 if [ "$1" = "--cli_only" ]; then
 	echo "installing comman line only"
@@ -23,15 +23,13 @@ case $1 in
     -w|--warm)
 	cp -rf terminator_warm ~/.config/terminator    
     shift # past argument
-    shift # past value
     ;;
     -c|--cold)
 	cp -rf terminator_cold ~/.config/terminator    
     shift # past argument
-    shift # past value
     ;;    
 esac
-done
+
 
 #let's install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -46,7 +44,7 @@ cd ~/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions
 
-echo "please log out and in or reboot the system"
+echo "Please log out and in or reboot the system"
 
 
 
